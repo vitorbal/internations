996dc8ddc5eced04b63802ffad48959bcfdbecef
Vitor Balocco <vitorbal@gmail.com>
2012-08-14 20:18:52 +0200

    Add group scaffold and add it's route to menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Internations::Application.routes.draw do
+
   root :to => 'users#index'
   resources :users
+  resources :groups
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120814175641) do
+ActiveRecord::Schema.define(:version => 20120814181541) do
+
+  create_table "groups", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
