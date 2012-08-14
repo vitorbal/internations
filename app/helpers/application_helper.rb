5f0b54150aeac2c50d32e963e6daaee3eef77fe3
Vitor Balocco <vitorbal@gmail.com>
2012-08-15 00:35:32 +0200

    Change menu helper to add active class to li instead

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
 module ApplicationHelper
-  # Wrapper around the link_to method which also sets
-  # "active" class if the path equals to the current page
+  # Menu helper which wraps a <li> tag around a link_to.
+  # Also sets the "active" class if the path equals to the current page
   def menu_link_to(label, path)
-    link_to label, path, :class => (current_page?(path) ? "active" : nil)
+    classString = current_page?(path) ? "active" : ""
+    result = "<li class='#{classString}'>" + link_to(label, path) + "</li>"
+
+    result.html_safe
   end
 end
