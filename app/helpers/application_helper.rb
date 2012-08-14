c004766ca1f33ca5d88f1185691ecdd79b0030b6
Vitor Balocco <vitorbal@gmail.com>
2012-08-14 22:05:37 +0200

    Add 'active' class to menu item of current page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  # Wrapper around the link_to method which also sets
+  # "active" class if the path equals to the current page
+  def menu_link_to(label, path)
+    link_to label, path, :class => (current_page?(path) ? "active" : nil)
+  end
 end
