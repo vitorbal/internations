164cb4846a7cf2d89426871803e97c3eaba8591c
Vitor Balocco <vitorbal@gmail.com>
2012-08-16 18:54:56 +0200

    Add uniqueness_of check to group model

--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -1,6 +1,7 @@
 class GroupUser < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
+  validates_uniqueness_of :user_id, :scope => :group_id
 
   # This is empty for now, but it could be useful in the future
   # if we want to add validations, callbacks or attributes to the
--- test/functional/groups_controller_test.rb
+++ test/functional/groups_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class GroupsControllerTest < ActionController::TestCase
   setup do
-    @group = groups(:one)
+    @group = groups(:developers)
   end
 
   test "should get index" do
@@ -39,11 +39,27 @@ class GroupsControllerTest < ActionController::TestCase
     assert_redirected_to group_path(assigns(:group))
   end
 
+  test "should update group through json" do
+    put :update, :format => :json, :id => @group.id, :group => { name: @group.name }
+    assert_response :success
+  end
+
+  test "should not add a user to users if he's already present" do
+    put :update, :format => :json, :id => @group.id, :group => {
+      name: @group.name,
+      user_ids: [ 1, 1, 2 ] }
+    assert_response :success
+    assert_equal @group.users.length, 2
+  end
+
   test "should destroy group" do
     assert_difference('Group.count', -1) do
-      delete :destroy, id: @group
+      # we check against an empty group
+      # because developers can't be destroyed since it has users
+      delete :destroy, id: groups(:germans)
     end
 
     assert_redirected_to groups_path
   end
+
 end
